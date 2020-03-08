c8d38fba08a4a02da76e2bd5ecb1a516a2118273
Viacheslav Nk <nk.viacheslav@gmail.com>
2020-03-08 22:57:12 +0200

    StreamBase

--- stream/StreamBaseEmpty.h
+++ stream/StreamBaseEmpty.h
@@ -24,44 +24,31 @@ namespace CommonLib
 		{}
 
 		//IStream
-		virtual uint32_t Size() const
+		virtual size_t Size() const
 		{
 			throw CExcBase("TMemoryStreamBaseEmpty: Size isn't implemented");
 		}
-
-		virtual uint64_t Size64() const
-		{
-			throw CExcBase("TMemoryStreamBaseEmpty: Size64 isn't implemented");
-		}
-
-		virtual void Seek(int32_t position, enSeekOffset offset)
+		
+		virtual void Seek(size_t position, enSeekOffset offset)
 		{
 			throw CExcBase("TMemoryStreamBaseEmpty: Seek isn't implemented");
 		}
-		virtual void Seek64(int64_t position, enSeekOffset offset)
-		{
-			throw CExcBase("TMemoryStreamBaseEmpty: Seek64 isn't implemented");
-		}
-		virtual uint32_t Pos() const
+		
+		virtual size_t Pos() const
 		{
 			throw CExcBase("TMemoryStreamBaseEmpty: Pos isn't implemented");
 		}
-		virtual uint64_t Pos64() const
-		{
-			throw CExcBase("TMemoryStreamBaseEmpty: Pos64 isn't implemented");
-		}
+		
 		virtual void Reset()
 		{
 			throw CExcBase("TMemoryStreamBaseEmpty: Reset isn't implemented");
+		
 		}
-		virtual void Attach(TStreamPtr pStream, int32_t nPos = -1, int32_t nSize = -1, bool bSeekPos = false)
+		virtual void Attach(TStreamPtr pStream, int64_t nPos = -1, int64_t nSize = -1, bool bSeekPos = false)
 		{
 			throw CExcBase("TMemoryStreamBaseEmpty: Attach isn't implemented");
 		}
-		virtual void Attach64(TStreamPtr pStream, int64_t nPos = -1, int64_t nSize = -1, bool bSeek = false)
-		{
-			throw CExcBase("TMemoryStreamBaseEmpty: Attach64 isn't implemented");
-		}
+			
 		virtual TStreamPtr Deattach()
 		{
 			throw CExcBase("TMemoryStreamBaseEmpty: Deattach isn't implemented");
@@ -73,7 +60,7 @@ namespace CommonLib
 
 
 		//IMemoryStream
-		virtual void AttachBuffer(byte_t* pBuffer, uint32_t nSize, bool bCopy = false)
+		virtual void AttachBuffer(byte_t* pBuffer, size_t nSize, bool bCopy = false)
 		{
 			throw CExcBase("TMemoryStreamBaseEmpty: AttachBuffer isn't implemented");
 		}
@@ -90,12 +77,12 @@ namespace CommonLib
 		{
 			throw CExcBase("TMemoryStreamBaseEmpty: Buffer isn't implemented");
 		}
-		virtual void Create(uint32_t nSize)
+		virtual void Create(size_t nSize)
 		{
 			throw CExcBase("TMemoryStreamBaseEmpty: Create isn't implemented");
 		}
 
-		virtual void Resize(uint32_t nSize) { throw CExcBase("TMemoryStreamBaseEmpty: resize isn't implemented"); }
+		virtual void Resize(size_t nSize) { throw CExcBase("TMemoryStreamBaseEmpty: resize isn't implemented"); }
 	protected:
 		bool m_bIsBigEndian;
 	};
